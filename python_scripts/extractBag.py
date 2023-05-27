b5c5aee56b852ea59fe22003785add0054b2e893
Shreyas Narasimhiah Ramesh <58805235+Shreyas-NR@users.noreply.github.com>
2023-05-27 02:23:20 -0700

    Update extractBag.py

--- python_scripts/extractBag.py
+++ python_scripts/extractBag.py
@@ -7,6 +7,10 @@ import cv2
 import os
 from PIL import Image
 
+filepath = '../inputs/20230525_202423.bag'   
+filename =  os.path.basename(filepath).split('.')[0]
+outdir = '../outputs/'+ os.path.basename(filepath).split('.')[0]
+
 # Possible values for color_scheme
 COLOR_SCHEMES = {
     'Jet': 0,
@@ -26,31 +30,31 @@ frames_list = []
 def create_output_directories(outdir):
     rgb_dir = os.path.join(outdir, 'rgb')
     depth_dir = os.path.join(outdir, 'depth')
-    fused_dir = os.path.join(outdir, 'fused')
+    aligned_rgbd_dir = os.path.join(outdir, 'aligned_rgbd')
 
     rgb_npy_dir = os.path.join(rgb_dir, 'npy')
     depth_npy_dir = os.path.join(depth_dir, 'npy')
-    fused_npy_dir = os.path.join(fused_dir, 'npy')
+    aligned_rgbd_npy_dir = os.path.join(aligned_rgbd_dir, 'npy')
 
     rgb_png_dir = os.path.join(rgb_dir, 'png')
     depth_png_dir = os.path.join(depth_dir, 'png')
-    fused_png_dir = os.path.join(fused_dir, 'png')
+    aligned_rgbd_png_dir = os.path.join(aligned_rgbd_dir, 'png')
 
     os.makedirs(outdir, exist_ok=True)
 
     os.makedirs(rgb_dir, exist_ok=True)
     os.makedirs(depth_dir, exist_ok=True)
-    os.makedirs(fused_dir, exist_ok=True)
+    os.makedirs(aligned_rgbd_dir, exist_ok=True)
 
     os.makedirs(rgb_npy_dir, exist_ok=True)
     os.makedirs(depth_npy_dir, exist_ok=True)
-    os.makedirs(fused_npy_dir, exist_ok=True)
+    os.makedirs(aligned_rgbd_npy_dir, exist_ok=True)
 
     os.makedirs(rgb_png_dir, exist_ok=True)
     os.makedirs(depth_png_dir, exist_ok=True)
-    os.makedirs(fused_png_dir, exist_ok=True)
+    os.makedirs(aligned_rgbd_png_dir, exist_ok=True)
 
-    return outdir, rgb_npy_dir, depth_npy_dir, fused_npy_dir, rgb_png_dir, depth_png_dir, fused_png_dir
+    return outdir, rgb_npy_dir, depth_npy_dir, aligned_rgbd_npy_dir, rgb_png_dir, depth_png_dir, aligned_rgbd_png_dir
 
 
 def save_frame_as_npy(frame, output_dir, frame_idx):
@@ -61,18 +65,18 @@ def save_frames_as_npy(frames, output_dir):
     for frame_idx, frame in enumerate(frames):
         save_frame_as_npy(frame, output_dir, frame_idx)
 
-def save_frame_as_png(frame, output_dir, frame_idx):
-    png_file = os.path.join(output_dir, f'{frame_idx+1}.png')
+def save_frame_as_png(frame, output_dir, frame_idx, type):
+    png_file = os.path.join(output_dir, filename + '_C1_' + type + '_' + str(frame_idx+1).zfill(3) + '.png')
     image = Image.fromarray(frame)
     image.save(png_file)
 
-def save_frames_as_png(frames, output_dir):
+def save_frames_as_png(frames, output_dir, type):
     for frame_idx, frame in enumerate(frames):
-        save_frame_as_png(frame, output_dir, frame_idx)
+        save_frame_as_png(frame, output_dir, frame_idx, type)
 
 def read_bag_file(filename, output_dir, save_npy=True, save_png=True, stream_frames=False):
     if not stream_frames:
-        out_dir, rgb_npy_dir, depth_npy_dir, fused_npy_dir, rgb_png_dir, depth_png_dir, fused_png_dir = create_output_directories(output_dir)
+        out_dir, rgb_npy_dir, depth_npy_dir, aligned_rgbd_npy_dir, rgb_png_dir, depth_png_dir, aligned_rgbd_png_dir = create_output_directories(output_dir)
 
     # Create pipeline
     pipeline = rs.pipeline()
@@ -127,7 +131,7 @@ def read_bag_file(filename, output_dir, save_npy=True, save_png=True, stream_fra
     depthColorizedFrames = []
     colorFrames = []
     depthFrames = []
-    fusedFrames = []        
+    alignedRgbdFrames = []        
     
     # Counter for loop iterations
     loop_counter = 0
@@ -137,30 +141,30 @@ def read_bag_file(filename, output_dir, save_npy=True, save_png=True, stream_fra
             # Get frameset of depth and color
             frames = pipeline.wait_for_frames()
             alignedFrames = align.process(frames)
-            color_frame_u = frames.get_color_frame()
+            color_frame_unaligned = frames.get_color_frame() #unaligned
             depth_frame = alignedFrames.get_depth_frame()
             color_frame = alignedFrames.get_color_frame()
 
             depth_image_colorized = np.asanyarray(colorizer.colorize(depth_frame).get_data())
             depth_frame = np.asanyarray(depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
-            color_image_u = np.asanyarray(color_frame_u.get_data())
+            color_image_unaligned = np.asanyarray(color_frame_unaligned.get_data())
             depth_frame = depth_frame.copy()
             depth_image_colorized = depth_image_colorized.copy()
             color_image = color_image.copy()
-            color_image_u = color_image_u.copy()
-            color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-            color_image_u_cvt = cv2.cvtColor(color_image_u, cv2.COLOR_BGR2RGB)
+            color_image_unaligned = color_image_unaligned.copy()
+            # color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+            color_image_u_cvt = cv2.cvtColor(color_image_unaligned, cv2.COLOR_BGR2RGB)
             fused = color_image + depth_image_colorized
             color_depth_fused = np.hstack((color_image_u_cvt, depth_image_colorized, fused))
             
             if not stream_frames:
                 # Append the color_depth_fused frame to the list
                 frames_list.append(color_depth_fused)
-                colorFrames.append(color_image_u)
+                colorFrames.append(color_image_unaligned)
                 depthFrames.append(depth_frame)
                 depthColorizedFrames.append(depth_image_colorized)
-                fusedFrames.append(fused)
+                alignedRgbdFrames.append(color_image)
                 # frames_list.append(color_depth_fused.copy())
                 # colorFrames.append(color_image.copy())
                 # depthFrames.append(depth_frame.copy())
@@ -188,17 +192,17 @@ def read_bag_file(filename, output_dir, save_npy=True, save_png=True, stream_fra
         # Stop streaming
         pipeline.stop()
         print("Pipeline stopped streaming")
-        print("Downloading NPY & PNG files to the output directory")
 
         if save_npy:
-            save_frames_as_npy(fusedFrames, fused_npy_dir)
+            print("Downloading NPY & PNG files to the output directory")
+            save_frames_as_npy(alignedRgbdFrames, aligned_rgbd_npy_dir)
             save_frames_as_npy(colorFrames, rgb_npy_dir)
             save_frames_as_npy(depthFrames, depth_npy_dir)
 
         if save_png:
-            save_frames_as_png(fusedFrames, fused_png_dir)
-            save_frames_as_png(colorFrames, rgb_png_dir)
-            save_frames_as_png(depthColorizedFrames, depth_png_dir)
+            save_frames_as_png(alignedRgbdFrames, aligned_rgbd_png_dir, type = 'rgbd')
+            save_frames_as_png(colorFrames, rgb_png_dir, type = 'rgb')
+            save_frames_as_png(depthColorizedFrames, depth_png_dir, type = 'depth')
 
 
 def write_frames_to_video(frames, output_file):
@@ -217,19 +221,17 @@ def write_frames_to_video(frames, output_file):
     out.release()
 
 def main():
-    filename = '../inputs/20230512_151113.bag'    
-    outdir = '../outputs/'+ os.path.basename(filename).split('.')[0]
 
     # Prompt the user to choose streaming or saving frames
     # choice = input("Enter '1' to stream frames or '2' to save frames: ")
-    choice = '2'
+    choice = '1'
 
     if choice == '1':
     # Stream frames
-        read_bag_file(filename, outdir, save_npy=False, save_png=False, stream_frames=True)
+        read_bag_file(filepath, outdir, save_npy=False, save_png=False, stream_frames=True)
     elif choice == '2':
         # Save frames
-        read_bag_file(filename, outdir, save_npy=True, save_png=True, stream_frames=False)
+        read_bag_file(filepath, outdir, save_npy=True, save_png=True, stream_frames=False)
 
         # Write the frames from the list to a video file
         output_file = outdir+'/color_depth_fused.avi'
